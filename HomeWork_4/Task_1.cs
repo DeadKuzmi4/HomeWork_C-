f2abe82a1a8bf4a932d096a378407f4874f88384
DeadKuzmi4 <maxim199819@yandex.ru>
2023-07-03 21:42:44 +0300

    Выполнил первое задание

---
+++ HomeWork_4/Task_1.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+int result = GetPow(A, B);
+Console.WriteLine($"{A} в степени {B} равно {result}");
+
+int GetPow(int A, int B){
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result*=A;
+    }
+    return result;
+}
