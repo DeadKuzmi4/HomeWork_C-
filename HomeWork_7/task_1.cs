1c6bf17f4f82ae320871071c2e09e5b891f05b21
DeadKuzmi4 <maxim199819@yandex.ru>
2023-07-14 16:11:16 +0300

    Выполнил первую задачу

---
+++ HomeWork_7/task_1.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите количество строк :");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n]; // Создаем двумерный массив
+
+Random random = new Random();
+
+// Заполняем массив случайными вещественными числами
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = random.NextDouble();
+    }
+}
+
+// Выводим содержимое массива
+Console.WriteLine("Сгенерированный массив:");
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j].ToString("0.00"));
+    }
+    Console.WriteLine();
+}
